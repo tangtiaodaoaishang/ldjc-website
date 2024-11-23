ebdde7fc8a1853dc6c4a2ea8f35100969e3601e2
tangtiaodaoaishang <2105525712@qq.com>
2024-11-23 15:24:39 +0800

    配置通义千文Al的代码(AIController类,properties配置文件)
    AIController类:
     private final ChatClient chatClient;
    
        public AIController(ChatClient.Builder builder) {
            this.chatClient = builder.build();
        }
    
        @GetMapping("/chat")
        public String chat(String message) {
            return this.chatClient.prompt()
                    .user(message)
                    .call()
                    .content();
        }
    
    properties配置文件
      --添加Al访问的key值
    spring.ai.dashscope.api-key=sk-83aec1d904cb4c92b970291d17b492fc

--- src/main/java/com/example/website/controller/IndexController.java
+++ src/main/java/com/example/website/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.example.website;
+package com.example.website.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
